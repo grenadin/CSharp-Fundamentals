ef17c89613a8f89faf3c233a8476aab504e4e4a3
HARIRAK SRITHAM <harirak@hotmail.co.th>
2020-04-29 01:39:07 +0700

    Learn String

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CsharpFundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Harirak";
+            var lastName = "Sritham";
+            var fullName = firstName + " " + lastName;
+            var myFullName = string.Format($"My name is {firstName} {lastName}");
+            Console.WriteLine(fullName);
+
+            var names = new string[3] {"Noon","Jane","Bow"};
+            Console.WriteLine(names[0]);
+            var formattedName = string.Join(", ", names);
+            Console.WriteLine(formattedName);
+
+            var text = @"Hi John
+Look into the following paths
+c:\folder\subfolder";
+            Console.WriteLine(text);
+        }
+
+    }
+}
